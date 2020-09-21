2a2f542fa4d895ada02ff1d9a9dee1c37a45b0c8
yen-tingLin <aillvone@gmail.com>
2020-09-21 20:08:57 +0800

    routing create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { CreateSubspringpostComponent } from './subspringpost/create-subspringpost/create-subspringpost.component';
+import { ListSubspringpostComponent } from './subspringpost/list-subspringpost/list-subspringpost.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'view-post/:id', component: ViewPostComponent },
+  { path: 'list-categories', component: ListSubspringpostComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-category', component: CreateSubspringpostComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent }
 ];
