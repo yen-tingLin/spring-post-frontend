587832b387811e3cf1c64c37534d8cf0a335c757
yen-tingLin <aillvone@gmail.com>
2020-09-22 14:04:39 +0800

    comment complete

---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommentPayload } from './comment-payload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCommentsForPost(postId: number): Observable<CommentPayload[]> {
+    return this.httpClient.get<CommentPayload[]>('http://localhost:8092/api/comments/by-post/' + postId);
+  }
+
+  postComment(commentPayload: CommentPayload): Observable<any> {
+    return this.httpClient.post<any>('http://localhost:8092/api/comments/create', commentPayload);
+  }
+
+  getCommentsByUser(username: string) {
+    return this.httpClient.get<CommentPayload[]>(
+          'http://localhost:8092/api/comments/by-user/' + username);
+  }
+
+}
